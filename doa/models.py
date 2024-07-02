f12e6be8107ce7380673b76e74d25546cbf5498f
ernawaty1228 <ernawaty1228@gmail.com>
2024-07-02 16:58:36 +0700

    membuat model nih

--- doa/models.py
+++ doa/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from kategori.models import Kategori
 
-# Create your models here.
+class Doa(models.Model):
+    judul = models.CharField(max_length=200)
+    arab = models.TextField()  
+    arti = models.TextField() 
+    kategori = models.ForeignKey(Kategori, related_name='doa', on_delete=models.CASCADE)
+    tanggal_dibuat = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.judul
